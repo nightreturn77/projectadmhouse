ad2272f6aa4b801de72245b0d31c5d78d722c2c5
Victor Dias <victordcporto@gmail.com>
2019-07-02 16:31:22 -0300

    1.2

--- index.php
+++ index.php
@@ -9,7 +9,10 @@ include('footer.php');
 
 <h2>Cadastrar produtos</h2>
 <form>
-<div>
-
-</div>
+    <div class="form-row">
+        <div class="form-group col-md-6">
+            <label for="nomeproduto">Nome</label>
+            <input type="text" class="form-control" id="nomeproduto" placeholder="Nome" name="nome">
+        </div>
+    </div>
 </form>

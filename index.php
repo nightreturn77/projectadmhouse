844acb6a35646279f7a0a23f2685d49bb2662243
Victor Dias <victordcporto@gmail.com>
2019-07-02 16:57:18 -0300

    1.3

--- index.php
+++ index.php
@@ -8,11 +8,29 @@ include('footer.php');
 ?>
 
 <h2>Cadastrar produtos</h2>
-<form>
+<form action="cadastrar.php" method="POST">
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="nomeproduto">Nome</label>
             <input type="text" class="form-control" id="nomeproduto" placeholder="Nome" name="nome">
         </div>
+        <div class="form-group col-md-6">
+            <label for="tipoproduto">Tipo</label>
+            <input type="text" class="form-control" id="tipoproduto" placeholder="Tipo" name="tipo">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="precoproduto">Preço</label>
+            <input type="text" class="form-control" id="precoproduto" placeholder="Preço" name="preco">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="validadeproduto">Validade</label>
+            <input type="date" class="form-control" id="validadeproduto" name="validade">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="dataproduto">Data da Compra</label>
+            <input type="date" class="form-control" id="dataproduto" name="dataCompra">
+        </div>
+
     </div>
+    <button type="submit" class="btn btn-primary">Cadastrar</button>
 </form>
